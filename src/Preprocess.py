302c1e2f611b4725c483922e2998aabb8374e47e
kelumsampath <edirisingheemks@gmail.com>
2020-11-19 07:56:52 +0530

    changed to bugs

--- src/Preprocess.py
+++ src/Preprocess.py
@@ -1,6 +1,6 @@
 import csv
 
-with open('./../dataset/mule.csv',  encoding="utf8") as csv_file:
+with open('./../dataset/spring.csv',  encoding="utf8") as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     word_array = [[]]
@@ -12,3 +12,8 @@ with open('./../dataset/mule.csv',  encoding="utf8") as csv_file:
             print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
             line_count += 1
     print(f'Processed {line_count} lines.')
+
+fields=['first','second','third']
+with open('test.csv', 'a',newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(fields)
