f630acad2cfe2ad0e11cb25728cb5d454be42821
kelumsampath <edirisingheemks@gmail.com>
2020-11-19 11:03:12 +0530

    input filtered dataset preprocessed

--- src/Preprocess.py
+++ src/Preprocess.py
@@ -26,12 +26,42 @@ with open('./../dataset/spring.csv',  encoding="utf8") as csv_file:
                 column_count +=1
         filtered_dataset.append([data_row[summary_column]] +[data_row[description_column]] +[data_row[assignee_column]]+[data_row[storypoint_column]]+ [data_row[i] for i in comment_column])
         line_count += 1
-    for row in filtered_dataset:
-        print(row)
-    print(f'Processed {line_count} lines.')
+        ##print filtered dataset
+    # for row in filtered_dataset:
+    #     print(row)
+    # print(f'Processed {line_count} lines.')
 
 with open('filtered_dataset.csv', 'w',newline='',encoding="utf-8") as f:
     writer = csv.writer(f)
     for row in filtered_dataset:
         writer.writerow(row)
+with open('input_processed_dataset.csv', 'w',newline='',encoding="utf-8") as f:
+    writer = csv.writer(f)
+    input_processed_dataset=[["Summary","Description","Number of developers","Number of comments","Story point"]]
+    row_count=0
+    for row in filtered_dataset:
+        single_entry = []
+        if row_count!=0:
+            single_entry.append(row[0])
+            single_entry.append(row[1])
+            assignee_count=0
+            if row[2]!="":
+                assignee_count=row[2].count(",")+1
+            single_entry.append(assignee_count)
+            comment_list = row[4: None]
+            comment_count=0
+            for comment in comment_list:
+                if comment != '':
+                    comment_count+=1
+            single_entry.append(comment_count)
+            single_entry.append(row[3])
+            ##print(single_entry)
+            input_processed_dataset.append(single_entry)
+        row_count +=1
 
+    ##print input processed datatset
+    for input_data_row in input_processed_dataset:
+        print(input_data_row)
+
+    for input_data_row in input_processed_dataset:
+        writer.writerow(input_data_row)
