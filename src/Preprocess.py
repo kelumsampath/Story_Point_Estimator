4a9d6a15dfe82ed220deb3ee42b59cffb895625f
kelumsampath <edirisingheemks@gmail.com>
2020-11-19 09:43:57 +0530

    filter required fields

--- src/Preprocess.py
+++ src/Preprocess.py
@@ -3,15 +3,29 @@ import csv
 with open('./../dataset/spring.csv',  encoding="utf8") as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
-    #word_array = [[]]
-    for row in csv_reader:
+    summary_column=0
+    description_column=0
+    assignee_column=0
+    comment_column=[]
+    storypoint_column=0
+    for data_row in csv_reader:
         if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
-            line_count += 1
-        else:
-            print(f'Column names are {", ".join(row)}')
-            #print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
-            line_count += 1
+            column_count=0
+            for column_name in data_row:
+                if column_name == "Summary" :
+                    summary_column=column_count
+                elif column_name == "Description":
+                    description_column=column_count
+                elif column_name == "Assignee":
+                    assignee_column=column_count
+                elif "Story Points" in column_name:
+                    storypoint_column=column_count
+                elif column_name == "Comment":
+                    comment_column.append(column_count)
+                column_count +=1
+       
+        print(" ".join([data_row[summary_column]] +[data_row[description_column]] +[data_row[assignee_column]]+[data_row[storypoint_column]]+ [data_row[i] for i in comment_column]))
+        line_count += 1
     print(f'Processed {line_count} lines.')
 
 fields=['first','second','third']
