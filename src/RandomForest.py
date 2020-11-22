7b475fdbbc87bd98cca945c482b443d18e02730f
kelumsampath <edirisingheemks@gmail.com>
2020-11-22 22:20:26 +0530

    finalize to get errer (mae)

--- src/RandomForest.py
+++ src/RandomForest.py
@@ -5,25 +5,29 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestRegressor
 from sklearn import metrics
 
-#dataset = pd.read_csv('../dataset/forrand/petrol_consumption.csv')
-dataset = pd.read_csv('./csv/5_tfidf_for_corpus.csv')
+tfidf_value_matrix = pd.read_csv('./csv/5_tfidf_for_corpus.csv')
 
-print(len(dataset.columns))
+number_of_columns = len(tfidf_value_matrix.columns)
 
-X = dataset.iloc[:, 0:3087].values
-y = dataset.iloc[:, 3087].values
+#tfidf value matrix(independent variables)
+X = tfidf_value_matrix.iloc[:, 0:number_of_columns-1].values
+#get story point list (dependent variable)
+y = tfidf_value_matrix.iloc[:, number_of_columns-1].values
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+#separate training set and test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
 
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
+
 regressor = RandomForestRegressor(n_estimators=20, random_state=0)
 regressor.fit(X_train, y_train)
 y_pred = regressor.predict(X_test)
-print(y_pred)
+##print(y_pred)
 
 print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
 print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))
 print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+
